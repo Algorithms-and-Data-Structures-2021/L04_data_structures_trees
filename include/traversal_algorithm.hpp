3e8d17091ea822b026a9f3503cf3098bbf2cf3c2
Ramil Safin <RamilSafNab1996@gmail.com>
2021-04-15 11:40:41 +0300

    Update traversal_algorithm.hpp

--- include/traversal_algorithm.hpp
+++ include/traversal_algorithm.hpp
@@ -6,29 +6,39 @@
 
 namespace itis {
 
+  // интерфейс алгоритма обхода узлов дерева
   struct TraversalAlgorithm {
     virtual ~TraversalAlgorithm() = default;
+    
+    /**
+     * Вывод ключей дерева в порядке, задаваемом стратегией обхода.  
+     * @param os - поток вывода 
+     * @param node - узел дерева (например, корневой) 
+     */
     virtual void Print(std::ostream &os, Node *node) const  = 0;
   };
 
+  // инфиксный обход узлов дерева
   struct InOrderTraversalAlgorithm final : TraversalAlgorithm {
     void Print(std::ostream &os, Node *node) const override;
   };
 
+  // префиксный обход узлов дерева
   struct PreOrderTraversalAlgorithm final : TraversalAlgorithm {
     void Print(std::ostream &os, Node *node) const override;
   };
 
+  // постфиксный обход узлов дерева
   struct PostOrderTraversalAlgorithm final : TraversalAlgorithm {
     void Print(std::ostream &os, Node *node) const override;
   };
 
-  // Level order traversal
+  // level order traversal (обход в ширину)
   struct BreadthFirstTraversalAlgorithm final : TraversalAlgorithm {
     void Print(std::ostream &os, Node *node) const override;
   };
 
-  // In-order, Pre-order, Post-order based on stack (non-recursive)
+  // обход в глубину на базе стека (итеративная реализация)
   struct DepthFirstTraversalAlgorithm final : TraversalAlgorithm {
     void Print(std::ostream &os, Node *node) const override;
   };
